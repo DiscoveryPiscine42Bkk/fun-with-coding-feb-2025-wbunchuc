a6d9674845c49d91511c15c1b79faf346e662648
A <antinonameth@gmail.com>
2025-02-08 21:05:02 +0700

    :D

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -2,46 +2,46 @@ $(document).ready(function() {
     const todoList = $("#ft_list");
     const newTodoBtn = $("#newTodoBtn");
 
-    loadTodos();
+    loadTodos(); 
 
     newTodoBtn.click(function() {
         const newTodo = prompt("Enter your new TO DO:");
         if (newTodo && newTodo.trim() !== "") {
-            addTodoToDOM(newTodo);
-            saveTodos();
+            addTodoToDOM(newTodo); 
+            saveTodos();// เก็บลงในlocalStorage
         }
     });
 
-    function addTodoToDOM(todo, isLoading = false) {
-        const todoDiv = $("<div>").addClass("todo").text(todo);
+    function addTodoToDOM(todo, isLoading = false) { //เช็คว่าโหลดจากlocalStorageมาไหม
+        const todoDiv = $("<div>").addClass("todo").text(todo); // สร้างdiv กำหนดให้มีclass todo และใส่ข้อความ TO DO
         
-        todoDiv.click(function() {
+        todoDiv.click(function() { //คลิกtodo
             const confirmRemove = confirm("Are you sure you want to remove this TO DO?");
             if (confirmRemove) {
                 todoDiv.remove();
-                saveTodos();
+                saveTodos(); // ลบ
             }
         });
 
-        if (isLoading) {
-            todoList.append(todoDiv);
+        if (isLoading) { //โหลดtodo
+            todoList.append(todoDiv);//ลบท้าย
         } else {
-            todoList.prepend(todoDiv);
+            todoList.prepend(todoDiv);//เพิ่มไวด้านบน
         }
     }
 
-    function saveTodos() {
-        const todos = [];
-        $(".todo").each(function() {
-            todos.push($(this).text());
+    function saveTodos() { //บันทึกลง localstoraage
+        const todos = []; // สร้าง arry
+        $(".todo").each(function() { //ลูป todo
+            todos.push($(this).text());//ดึกข้อมูลใน todo ลงในarry
         });
-        localStorage.setItem("todos", JSON.stringify(todos));
+        localStorage.setItem("todos", JSON.stringify(todos));//arryลง local ในรูปแบบ Str
     }
 
-    function loadTodos() {
+    function loadTodos() { //โหลดจาก local
         const todos = JSON.parse(localStorage.getItem("todos")) || [];
         if (todos.length > 0) {
-            todos.forEach(todo => addTodoToDOM(todo, true));
+            todos.forEach(todo => addTodoToDOM(todo, true));// ลงหน้าเว็ป
         }
     }
 });
